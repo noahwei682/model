3c19b041286e7ec8e937355202b76b884518b554
Noah682 <noahwei682@gmail.com>
2025-02-22 16:11:44 +0800

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -5,16 +5,15 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-export PYTHONPATH=/home/aiscuser/mycode/llava_clipSingleText/llava_aoqi
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export FORCE_TORCHRUN=8
 deepspeed --num_gpus=8 llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version plain \
-    --data_path /blob/weiwei/codes/llava_aoqi/playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
-    --text_embedding_path /blob/weiwei/text_embedding/blip_laion_cc_sbu_558k.pt \
-    --image_folder /blob/weiwei/codes/llava_dynaCrossClipOfflinetext/llava_aoqi/playground/data/LLaVA-Pretrain/images \
+    --data_path $PRE_DATA \
+    --text_embedding_path $PRE_EMB \
+    --image_folder $PRE_IMAGE \
     --vision_tower EVA02-CLIP-L-14-336 \
     --eva_ckpt_path $EVA_CKPT \
     --run_name llava-v1.5-7b-$CKPT_DIR-pretrain \
@@ -24,7 +23,7 @@ deepspeed --num_gpus=8 llava/train/train_mem.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir /blob/weiwei/llava_checkpoint/llava-v1.5-7b-$CKPT_DIR-pretrain \
+    --output_dir $OUT_DIR_REPFIX-$CKPT_DIR-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
