36ad568ef56c47f99fe2c20ec6c76c49f461af26
Noah682 <noahwei682@gmail.com>
2025-02-22 16:17:29 +0800

    Update finetune.sh

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -5,20 +5,19 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-export PYTHONPATH=/home/aiscuser/mycode/llava_clipSingleText/llava_aoqi
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export FORCE_TORCHRUN=8
 deepspeed --num_gpus=8 llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path /blob/weiwei/codes/llava_aoqi/playground/data/llava_v1_5_mix665k.json \
-    --text_embedding_path /blob/weiwei/text_embedding/llava_v1_5_mix665k.pt \
-    --image_folder /blob/weiwei/codes/llava_aoqi/playground/data \
+    --data_path $FINE_DATA \
+    --text_embedding_path $FINE_EMB \
+    --image_folder $FINE_IMAGE \
     --vision_tower EVA02-CLIP-L-14-336  \
     --eva_ckpt_path $EVA_CKPT \
     --run_name llava-v1.5-7b-$CKPT_DIR \
-    --pretrain_mm_mlp_adapter /blob/weiwei/llava_checkpoint/llava-v1.5-7b-$CKPT_DIR-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter $MLP_CKPT_PREFIX-$CKPT_DIR-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -26,7 +25,7 @@ deepspeed --num_gpus=8 llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /blob/weiwei/llava_checkpoint/llava-v1.5-7b-$CKPT_DIR \
+    --output_dir $MODEL_CKPT_DIR-$CKPT_DIR \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
